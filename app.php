d3055e28e346e4f82bdbb301ba53d0ae61334bf9
anaescanez <47746568+anaescanez@users.noreply.github.com>
2019-11-27 15:58:44 -0300

    Commit 27/11

--- app.php
+++ app.php
@@ -3,5 +3,5 @@ require_once 'biblioteca/acesso.php';
 require_once 'biblioteca/alert.php';
 
 define('CONTROLADOR_PADRAO', 'paginas'); //por padrão vem definido o controlador de paginas estáticas (paginasControlador)
-//define('URL_BASE', 'http://loja-world-of-books.000webhostapp.com/');
-define('URL_BASE', 'http://localhost/loja/');
+define('URL_BASE', 'http://loja-world-of-books.000webhostapp.com/');
+//define('URL_BASE', 'http://localhost/loja/');
